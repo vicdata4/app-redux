5c3575cfd09b179bc685cc2a12d6501a51d08269
victorgh <victor.gonzalez@soprasteria.com>
2019-05-17 11:00:28 +0200

    feat: middlewares in redux (redux thunk) shortened sintax / async actions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
-import App from './components/App'
+import App from './components/App';
+
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
